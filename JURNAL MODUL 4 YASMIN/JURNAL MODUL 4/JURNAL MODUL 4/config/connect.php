48d6631f397b4e2bb70d902fd2a1a5511aa005de
yasminaliviaa <yasmin.alivia99@gmail.com>
2023-11-23 08:15:10 +0700

    modul 4

---
+++ JURNAL MODUL 4 YASMIN/JURNAL MODUL 4/JURNAL MODUL 4/config/connect.php
@@ -0,0 +1,19 @@
+<!-- File ini berisi koneksi dengan database MySQL -->
+<?php 
+
+// (1) Buatlah variable untuk connect ke database yang telah di import ke phpMyAdmin
+$db = mysqli_connect("localhost:3306", "root", "", "modul4");
+
+
+// 
+
+// (2) Buatlah perkondisian untuk menampilkan pesan error ketika database gagal terkoneksi
+if (!$db) {
+    die("Koneksi gagal: " .mysqli_connect_error());
+
+}
+// mysqli_close($db);
+
+// 
+ 
+?>
